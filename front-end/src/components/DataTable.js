ee5339786786a17105a8f918e98f5c54faa56e6b
Akul-sada <sadananda.sherigara@gmail.com>
2023-06-11 16:09:27 +0530

    change date format of the date in  DataTable.js

--- front-end/src/components/DataTable.js
+++ front-end/src/components/DataTable.js
@@ -1,7 +1,20 @@
 import React from 'react'
 
 const DataTable = ({data}) => {
-  
+  function changeDateFormat(inputDate){
+    // Create a new Date object using the input date string
+    const date = new Date(inputDate);
+
+    // Define the options for formatting the date
+    const options ={
+      day:"numeric",
+      month:"long",
+      year:"numeric"
+    }
+    const formatedDate = date.toLocaleDateString("en-US",options);
+    return formatedDate;
+
+  }
   return (
     <>
       <div className='flex justify-center'>
@@ -24,7 +37,7 @@ const DataTable = ({data}) => {
               <td className="border px-4 py-2 text-center">{i+1}</td>
               <td className="border px-4 py-2 text-center">{data.name}</td>
               <td className="border px-4 py-2 text-center">{JSON.stringify(data.is_vaccinated)}</td>
-              <td className="border px-4 py-2 text-center">{data.birthdate}</td>
+              <td className="border px-4 py-2 text-center">{changeDateFormat(data.birthdate)}</td>
               <td className="border px-4 py-2 text-center">{data.gender}</td>
             </tr>)} 
           </tbody>
